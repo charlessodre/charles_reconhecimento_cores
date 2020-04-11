d9b05d5a99a7160e1620457fd540badcaa52d058
Charles Sodré <charlessodre@gmail.com>
2020-04-11 12:48:40 -0300

    Ajuste Range Cores

--- reconhecimento_cores.py
+++ reconhecimento_cores.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import cv2
 import numpy as np
 
@@ -6,33 +7,40 @@ print(cv2.__version__)
 # define range colors in HSV
 
 # vermelho
-lower_red = np.array([168, 175, 173])
-upper_red = np.array([191, 234, 255])
+lower_red = np.array([158, 232, 147])
+upper_red = np.array([198, 255, 226])
 
 # laranja
-lower_orange = np.array([0, 182, 45])
-upper_orange = np.array([10, 222, 240])
+lower_orange = np.array([175, 149, 184])
+upper_orange = np.array([210, 212, 230])
 
 # verde
-lower_green = np.array([66, 136, 93])
-upper_green = np.array([83, 236, 144])
+lower_green = np.array([51, 107, 128])
+upper_green = np.array([82, 255, 190])
 
 # azul
-lower_blue = np.array([101, 130, 80])
-upper_blue = np.array([128, 255, 188])
+lower_blue = np.array([81, 138, 133])
+upper_blue = np.array([109, 219, 219])
 
 # amarelo
 lower_yellow = np.array([4, 181, 88])
 upper_yellow = np.array([24, 255, 255])
 
 # roxo
-lower_purple = np.array([104, 107, 105])
-upper_purple = np.array([131, 280, 143])
+lower_purple = np.array([110, 88, 120])
+upper_purple = np.array([156, 159, 201])
 
 # rosa
-lower_pink = np.array([160, 172, 112])
-upper_pink = np.array([172, 255, 236])
+lower_pink = np.array([153, 112, 183])
+upper_pink = np.array([188, 174, 248])
 
+# branco
+lower_white = np.array([0, 0, 255])
+upper_white = np.array([255, 255, 255])
+
+# marrom
+lower_brown = np.array([157, 74, 0])
+upper_brown = np.array([223, 216, 70])
 
 def get_moments_centroid(moments):
     return (int(moments["m10"] / moments["m00"]), int(moments["m01"] / moments["m00"]))
@@ -71,7 +79,14 @@ def color_tracking(image, color_mask, contour_color, color_name):
         cv2.putText(image, color_name, centroid, cv2.FONT_HERSHEY_PLAIN, 1, contour_color)
 
 
+# Caputura o video
 webcam = cv2.VideoCapture(0)
+_, frame = webcam.read()
+
+# Define as configurações para salvar o frame
+output_video = "./output/saida_video.mp4"
+save_frame = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*'mp4v'), 10,
+                             (frame.shape[1], frame.shape[0]))
 
 if not webcam.isOpened():
     print("Could not open webcam!")
@@ -90,6 +105,8 @@ while True:
     pink_mask = get_color_mask(frame_hsv, lower_pink, upper_pink)
     purple_mask = get_color_mask(frame_hsv, lower_purple, upper_purple)
     orange_mask = get_color_mask(frame_hsv, lower_orange, upper_orange)
+    white_mask = get_color_mask(frame_hsv, lower_white, upper_white)
+    brown_mask = get_color_mask(frame_hsv, lower_brown, upper_brown)
 
     color_tracking(frame, blue_mask, (255, 0, 0), 'azul')
     color_tracking(frame, green_mask, (0, 255, 0), 'verde')
@@ -97,13 +114,17 @@ while True:
     color_tracking(frame, yellow_mask, (0, 255, 255), 'amarelo')
     color_tracking(frame, pink_mask, (255, 0, 255), 'rosa')
     color_tracking(frame, purple_mask, (255, 85, 170), 'roxo')
-    color_tracking(frame, orange_mask, (85, 85, 255), 'laranja')
+    color_tracking(frame, orange_mask, (0, 102, 255), 'laranja')
+    color_tracking(frame, white_mask, (255, 255, 255), 'branco')
+    color_tracking(frame, brown_mask, (0, 51, 102), 'marron')
 
     cv2.imshow("video", frame)
+    save_frame.write(frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # Release all resources.
+save_frame.release()
 webcam.release()
 cv2.destroyAllWindows()
